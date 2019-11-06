00abd3f636374948eb8a4f1e6ce536856d2f4619
Jason Maa <jasonmaa3955@gmail.com>
2019-11-06 18:37:38 -0500

    V2 (#9)
    * Switching to Django 2
    
    * Room joining working
    
    * Convert to channels2
    
    * Remove most jquery
    
    * Fix docker deployment
    
    * Fix leave

--- web/game/consumers.py
+++ web/game/consumers.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from django.db.models import Q
-from channels import Group
-from channels.sessions import channel_session
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
 from .models import *
 from .utils import *
 
@@ -15,85 +15,165 @@ from fuzzywuzzy import fuzz
 
 GRACE_TIME = 3
 
-@channel_session
-def ws_connect(message):
-    # get room
-    prefix, label = message['path'].strip('/').split('/')
-    room = Room.objects.get(label=label)
-    Group('game-'+label).add(message.reply_channel)
-    message.reply_channel.send({"accept":True})
-    message.channel_session['room'] = room.label
+class QuizbowlConsumer(AsyncJsonWebsocketConsumer):
+    """Websocket consumer for quizbowl game
+    """
 
-@channel_session
-def ws_receive(message):
-    # get message
-    label = message.channel_session['room']
-    room = Room.objects.get(label=label)
-    data = json.loads(message['text'])
+    async def connect(self):
+        """Weboscket connect
+        """
+        self.room_name = self.scope['url_route']['kwargs']['label']
+        self.room_group_name = f"game-{self.room_name}"
 
-    # determine request type
-    if(data['request_type'] == 'ping'):
-        # update ping
+        # Join room
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+    
+    async def disconnect(self, close_code):
+        """Websocket disconnect
+        """
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        """Websocket receive
+        """
+        data = json.loads(text_data)
+        room = Room.objects.get(label=self.room_name)
+
+        if data['request_type'] == 'ping':
+            await self.ping(room, data)
+        elif data['request_type'] == 'join':
+            await self.join(room, data)
+        elif data['request_type'] == 'leave':
+            await self.leave(room, data)
+        elif data['request_type'] == 'get_answer':
+            await self.get_answer(room, data)
+        elif data['request_type'] == 'new_user':
+            await self.new_user(room)
+        elif data['request_type'] == 'set_name':
+            await self.set_name(room, data)
+        elif data['request_type'] == 'next':
+            await self.next(room, data)
+        elif data['request_type'] == 'buzz_init':
+            await self.buzz_init(room, data)
+        elif data['request_type'] == 'buzz_answer':
+            await self.buzz_answer(room, data)
+        elif data['request_type'] == 'set_category':
+            await self.set_category(room, data)
+        elif data['request_type'] == 'set_difficulty':
+            await self.set_difficulty(room, data)
+        elif data['request_type'] == 'reset_score':
+            await self.reset_score(room, data)
+        elif data['request_type'] == 'chat':
+            await self.chat(room, data)
+        else:
+            pass
+
+
+    async def update_room(self, event):
+        """Room update handler
+        """
+        await self.send_json(event['data'])
+
+    async def ping(self, room, data):
+        """Receive ping
+        """
         try:
             p = room.players.get(player_id=data['player_id'])
             p.last_seen = datetime.datetime.now().timestamp()
             p.save()
 
             update_time_state(room)
-            message.reply_channel.send(get_response_json(room))
+                    
+            await self.send_json(get_response_json(room))
+
         except Exception as e:
-            p = create_new_user(room)
+            await self.new_user(room)
 
-            message.reply_channel.send({'text':json.dumps({
-                "response_type":"new_user",
-                "player_id":p.player_id,
-                "player_name":p.name,
-            })})
+    async def join(self, room, data):
+        """Join room
+        """
+        p = room.players.get(player_id=data['player_id'])
+        create_message("join", f"<strong>{p.name}</strong> has joined the room", room)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update_room',
+                'data': get_response_json(room),
+            }
+        )
 
-            create_message("join", f"<strong>{p.name}</strong> has joined the room", room)
-            Group('game-'+label).send(get_response_json(room))
-
-    elif(data['request_type'] == 'join'):
-            p = room.players.get(player_id=data['player_id'])
-            create_message("join", f"<strong>{p.name}</strong> has joined the room", room)
-            Group('game-'+label).send(get_response_json(room))
-
-    elif(data['request_type'] == 'leave'):
+    async def leave(self, room, data):
+        """Leave room
+        """
         p = room.players.get(player_id=data['player_id'])
         create_message("leave", f"<strong>{p.name}</strong> has left the room", room)
-        Group('game-'+label).send(get_response_json(room))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update_room',
+                'data': get_response_json(room),
+            }
+        )
 
-    elif(data['request_type'] == 'new_user'):
-        # new user
+    async def new_user(self, room):
+        """Create new player
+        """
         p = create_new_user(room)
 
-        message.reply_channel.send({'text':json.dumps({
-            "response_type":"new_user",
-            "player_id":p.player_id,
-            "player_name":p.name,
-        })})
-
+        await self.send_json({
+            "response_type": "new_user",
+            "player_id": p.player_id,
+            "player_name": p.name,
+        })
         create_message("join", f"<strong>{p.name}</strong> has joined the room", room)
-        Group('game-'+label).send(get_response_json(room))
 
-    elif(data['request_type'] == 'set_name'):
-        # update name
-        p = Player.objects.get(player_id=int(data['player_id']))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update_room',
+                'data': get_response_json(room),
+            }
+        )
+
+    async def set_name(self, room, data):
+        """Update player name
+        """
+        p = Player.objects.get(player_id=data['player_id'])
         old_name = p.name
         p.name = clean_content(data['content'])
         try:
             p.full_clean()
             p.save()
 
-            Group('game-'+label).send(get_response_json(room))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': get_response_json(room),
+                }
+            )
+
         except ValidationError as e:
             return
 
-    elif(data['request_type'] == 'next'):
-        # next question
+    async def next(self, room, data):
+        """Next question
+        """
         update_time_state(room)
         if room.state == 'idle':
-            questions = Question.objects.filter(difficulty=room.difficulty) if room.category == 'Everything' else Question.objects.filter(Q(category=room.category) & Q(difficulty=room.difficulty))
+            questions = (
+                Question.objects.filter(difficulty=room.difficulty)
+                if room.category == 'Everything'
+                else Question.objects.filter(Q(category=room.category) & Q(difficulty=room.difficulty))
+            )
             q = random.choice(questions)
 
             room.state = 'playing'
@@ -108,11 +188,17 @@ def ws_receive(message):
                 p.locked_out = False
                 p.save()
 
-            Group('game-'+label).send(get_response_json(room))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': get_response_json(room),
+                }
+            )
 
-    elif(data['request_type'] == 'buzz_init'):
-        # buzz init
-
+    async def buzz_init(self, room, data):
+        """Initialize buzz
+        """
         p = room.players.get(player_id=data['player_id'])
 
         if not p.locked_out and room.state == 'playing':
@@ -122,11 +208,15 @@ def ws_receive(message):
             room.save()
 
             create_message("buzz_init", f"<strong>{p.name}</strong> has buzzed", room)
-            Group('game-'+label).send(get_response_json(room))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': get_response_json(room),
+                }
+            )
 
-    elif(data['request_type'] == 'buzz_answer'):
-        # buzz answer
-
+    async def buzz_answer(self, room, data):
         p = room.players.get(player_id=data['player_id'])
 
         if p.player_id == room.buzz_player.player_id and room.state == 'contest':
@@ -142,7 +232,11 @@ def ws_receive(message):
                 room.end_time = room.start_time
                 room.save()
 
-                create_message("buzz_correct", f"<strong>{p.name}</strong> correctly answered <strong><i>{cleaned_content}</i></strong>", room)
+                create_message(
+                    "buzz_correct",
+                    f"<strong>{p.name}</strong> correctly answered <strong><i>{cleaned_content}</i></strong>",
+                    room
+                )
             else:
                 # question reading ended, do penalty
                 if room.end_time - room.buzz_start_time >= GRACE_TIME:
@@ -150,11 +244,15 @@ def ws_receive(message):
                 p.locked_out = True
                 p.save()
 
-                create_message("buzz_wrong", f"<strong>{p.name}</strong> incorrectly answered <strong><i>{cleaned_content}</i></strong>", room)
+                create_message(
+                    "buzz_wrong",
+                    f"<strong>{p.name}</strong> incorrectly answered <strong><i>{cleaned_content}</i></strong>",
+                    room
+                )
 
-                message.reply_channel.send({'text':json.dumps({
+                await self.send_json({
                     "response_type":"lock_out",
-                })})
+                })
 
             buzz_duration = datetime.datetime.now().timestamp() - room.buzz_start_time
             room.state = 'playing'
@@ -162,9 +260,17 @@ def ws_receive(message):
             room.end_time += buzz_duration
             room.save()
 
-            Group('game-'+label).send(get_response_json(room))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': get_response_json(room),
+                }
+            )
 
-    elif(data['request_type'] == 'get_answer'):
+    async def get_answer(self, room, data):
+        """Get answer for room question
+        """
         update_time_state(room)
         if room.state == 'idle':
             # generate random question for now if empty
@@ -173,13 +279,21 @@ def ws_receive(message):
                 q = random.choice(questions)
                 room.current_question = q
                 room.save()
+            
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': {
+                        "response_type": "send_answer",
+                        "answer": room.current_question.answer,
+                    },
+                }
+            )
 
-            Group('game-'+label).send({'text':json.dumps({
-                "response_type":"send_answer",
-                "answer":room.current_question.answer,
-            })});
-
-    elif(data['request_type'] == 'set_category'):
+    async def set_category(self, room, data):
+        """Set room category
+        """
         try:
             room.category = clean_content(data['content'])
             room.current_question = random.choice(Question.objects.all()) if room.current_question == None else room.current_question
@@ -188,57 +302,86 @@ def ws_receive(message):
             room.save()
 
             create_message("set_category", f"The category is now <strong><i>{room.category}</i></strong>", room)
-            Group('game-'+label).send(get_response_json(room))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': get_response_json(room),
+                }
+            )
         except ValidationError as e:
             pass
 
-    elif(data['request_type'] == 'set_difficulty'):
+    async def set_difficulty(self, room, data):
+        """Set room difficulty
+        """
         try:
             room.difficulty = clean_content(data['content'])
-            room.current_question = random.choice(Question.objects.all()) if room.current_question == None else room.current_question
+            room.current_question = (
+                random.choice(Question.objects.all())
+                if room.current_question == None
+                else room.current_question
+            )
             room.buzz_player = room.players.first()
             room.full_clean()
             room.save()
 
             create_message("set_difficulty", f"The difficulty is now <strong><i>{room.difficulty}</i></strong>", room)
-            Group('game-'+label).send(get_response_json(room))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'update_room',
+                    'data': get_response_json(room),
+                }
+            )
         except ValidationError as e:
             pass
 
-    elif(data['request_type'] == 'reset_score'):
+
+    async def reset_score(self, room, data):
+        """Reset player score
+        """
         p = room.players.get(player_id=data['player_id'])
         p.score = 0
         p.save()
 
         create_message("reset_score", f"<strong>{p.name}</strong> has reset their score", room)
-        Group('game-'+label).send(get_response_json(room))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update_room',
+                'data': get_response_json(room),
+            }
+        )
 
-    elif(data['request_type'] == 'chat'):
+    async def chat(self, room, data):
         p = room.players.get(player_id=data['player_id'])
         m = clean_content(data['content'])
 
         create_message("chat", f"<strong>{p.name}</strong>: {m}", room)
-        Group('game-'+label).send(get_response_json(room))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update_room',
+                'data': get_response_json(room),
+            }
+        )
 
-
-@channel_session
-def ws_disconnect(message):
-    label = message.channel_session['room']
-    Group('game-'+label).discard(message.reply_channel)
 
 # === Helper methods ===
 
 def update_time_state(room):
-    """Checks time and updates state"""
+    """Checks time and updates state
+    """
     if not room.state == 'contest':
         if datetime.datetime.now().timestamp() >= room.end_time:
             room.state = 'idle'
             room.save()
 
 def get_response_json(room):
-    """Generates json for update response"""
-
-    return {'text':json.dumps({
+    """Generates json for update response
+    """
+    return {
         "response_type":"update",
         "game_state":room.state,
         "current_time":datetime.datetime.now().timestamp(),
@@ -251,10 +394,11 @@ def get_response_json(room):
         "scores":room.get_scores(),
         "messages":room.get_messages(),
         "difficulty":room.difficulty,
-    })}
+    }
 
 def create_message(tag, content, room):
-    """Adds a message to db"""
+    """Adds a message to db
+    """
     try:
         m = Message(content=content, room=room, tag=tag)
         m.full_clean()
@@ -263,7 +407,8 @@ def create_message(tag, content, room):
         return
 
 def create_new_user(room):
-    """Generates new user"""
+    """Generates new user
+    """
     # generate new id
     m = hashlib.md5()
     m.update((room.label + str(datetime.datetime.now().timestamp())).encode("utf8"))
