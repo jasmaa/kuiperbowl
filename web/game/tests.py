569860654eb726704efc76fc4a6ee75da6a2bdb7
jasmaa <jasonmaa3955@gmail.com>
2020-05-29 16:19:32 -0400

    Test unban

--- web/game/tests.py
+++ web/game/tests.py
@@ -7,7 +7,9 @@ class TestRoom:
     
     def test_player_count(self):
         test_room = Room.objects.get(label='testroom')
-        assert test_room.players.count() == 2
+        assert test_room.players.count() == 3
+        assert test_room.players.filter(player_id=1).count() == 1
+        assert test_room.players.filter(player_id=3).count() == 1
 
     def test_get_scores(self):
         test_room = Room.objects.get(label='testroom')
@@ -16,3 +18,12 @@ class TestRoom:
     def test_get_messages(self):
         test_room = Room.objects.get(label='testroom')
         assert test_room.get_messages() == []
+    
+    def test_unban(self):
+        test_room = Room.objects.get(label='testroom')
+        banned_player = test_room.players.get(player_id=3)
+        assert banned_player.banned
+        assert banned_player.reported_by.count() == 2
+        banned_player.unban()
+        assert not banned_player.banned
+        assert banned_player.reported_by.count() == 0
