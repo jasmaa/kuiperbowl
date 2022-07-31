17cda23e7ce20dadb79f817e41963ca92e4c40e1
jasmaa <jasonmaa3955@gmail.com>
2022-07-31 12:07:37 -0400

    Revert asgi.py to pre Django 4 upgrade. Add daphne and conditional install for twisted-iocpsupport.

--- web/quizbowl/asgi.py
+++ web/quizbowl/asgi.py
@@ -1,25 +1,7 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizbowl.settings")
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
-import quizbowl.routing
-
-application = ProtocolTypeRouter({
-  "http": django_asgi_app,
-  "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                quizbowl.routing.websocket_urlpatterns
-            )
-        )
-    ),
-})
+django.setup()
+application = get_default_application()
