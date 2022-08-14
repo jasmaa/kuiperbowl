e30ad617ff884397ecf5d7139280802d2fda9188
jasmaa <jasonmaa3955@gmail.com>
2022-08-14 10:07:02 -0400

    Redirect to game page when Go button clicked on home page

--- web/game/static/game/home.js
+++ web/game/static/game/home.js
@@ -3,18 +3,27 @@
 
 const landingContent = document.getElementById('landing-content');
 const landingForm = document.getElementById('landing-form');
+const landingButton = document.getElementById('landing-btn')
 
-landingForm.onsubmit = (e) => {
-
-  e.preventDefault();
-  e.stopPropagation();
-
+function submitGamePage() {
   const loc = landingContent.value.trim();
-  
+
   if (loc != '' && loc.match(/^[a-z0-9_-]+$/)) {
     window.location.href = "/game/" + loc;
   }
   else {
     landingContent.classList.add('is-invalid');
   }
+}
+
+landingForm.onsubmit = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  submitGamePage();
+}
+
+landingButton.onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  submitGamePage();
 }
