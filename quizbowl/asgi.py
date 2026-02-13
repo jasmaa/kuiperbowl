6afe342a58c22a18c52d96dfaa1f823df0fb5666
jasmaa <jasonmaa3955@gmail.com>
2026-02-13 09:54:17 -0500

    fix: Fix for daphne

--- quizbowl/asgi.py
+++ quizbowl/asgi.py
@@ -3,13 +3,15 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import game.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizbowl.settings")
-django.setup()
+asgi_app = get_asgi_application()
+
+import game.routing
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(game.routing.websocket_urlpatterns)
         ),
