ab9e59a6de66e0167fb0459ce0b32a6e9f881623
jasmaa <jasonmaa3955@gmail.com>
2026-02-13 09:38:51 -0500

    chore: Upgrade deps and fix websocket routing

--- quizbowl/asgi.py
+++ quizbowl/asgi.py
@@ -1,7 +1,17 @@
 import os
 import django
-from channels.routing import get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+import game.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizbowl.settings")
 django.setup()
-application = get_default_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(game.routing.websocket_urlpatterns)
+        ),
+    }
+)
